0260914afd10814f40f6aa93db152fb5e04527a0
zhousicheng <939551865@qq.com>
2021-11-28 15:42:41 +0800

    ball run 1
    The ball moves left and right

--- prj1/src/com/company/BallGame.java
+++ prj1/src/com/company/BallGame.java
@@ -1,4 +1,6 @@
 package com.company;
+import com.sun.deploy.security.SelectableSecurityManager;
+
 import javax.swing.JFrame;
 import java.awt.*;
 
@@ -9,12 +11,25 @@ public class BallGame extends JFrame {
     Image desk = Toolkit.getDefaultToolkit().getImage("images/desk.png");
     double x=100;
     double y=100;
+    boolean right = true;
 
     //画窗口的方法，加载小球与桌面
     public void paint(Graphics g){
         System.out.println("窗口画一次");
         g.drawImage(desk,0,0,null);
         g.drawImage(ball,(int)x,(int)y,null);
+
+        if(right){
+            x=x+10;
+        }else {
+            x=x-10;
+        }
+        if(x>856-40-30){
+            right=false;
+        }
+        if (x<40){
+            right=true;
+        }
     }
 
     //窗口加载
@@ -22,7 +37,16 @@ public class BallGame extends JFrame {
         setSize(856,500);
         setLocation(50,50);
         setVisible(true);
+        while (true){
+            repaint();
+            try{
+                Thread.sleep(40);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
     }
+
     public static void main(String[] args){
         System.out.println("2");
         BallGame game = new BallGame();
