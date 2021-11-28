e8022ccce0ab74f72870d31e09d104369955ee55
zhousicheng <939551865@qq.com>
2021-11-28 15:56:18 +0800

    ball run2
    use degree 
    delete right

--- prj1/src/com/company/BallGame2.java
+++ prj1/src/com/company/BallGame2.java
@@ -1,16 +1,19 @@
 package com.company;
+import com.sun.deploy.security.SelectableSecurityManager;
 
 import javax.swing.JFrame;
 import java.awt.*;
 
 
-public class BallGame extends JFrame {
-
+public class BallGame2 extends JFrame {
+    //添加小球的和桌面图片的路径
     Image ball = Toolkit.getDefaultToolkit().getImage("images/ball.png");
     Image desk = Toolkit.getDefaultToolkit().getImage("images/desk.png");
-    double x=100;
-    double y=100;
-    boolean right = true;
+
+    //指定小球的初始位置
+    double x=100;//小球的横坐标
+    double y=100;//小球的纵坐标
+    double degree =3.14/3;//弧度：小球运动的角度，此处为60度
 
     //画窗口的方法，加载小球与桌面
     public void paint(Graphics g){
@@ -18,17 +21,20 @@ public class BallGame extends JFrame {
         g.drawImage(desk,0,0,null);
         g.drawImage(ball,(int)x,(int)y,null);
 
-        if(right){
-            x=x+10;
-        }else {
-            x=x-10;
+        //根据角度degree改变小球的坐标
+        x = x+10*Math.cos(degree);
+        y = y+10*Math.sin(degree);
+        //边界检测：碰到上下边界
+        //500是窗口的高度，40是桌面的边框，30球的直径，最后一个40是标题栏的高度
+        if(y>500-40-30||y<40+40){
+            degree=-degree;
         }
-        if(x>856-40-30){
-            right=false;
+        //边界检测：碰到左右边界
+        //856是窗口的高度，40是桌子的边框，30是球的直径
+        if (x<40||x>856-40-30){
+            degree=3.14-degree;
         }
-        if (x<40){
-            right=true;
-        }
+
     }
 
     //窗口加载
@@ -48,7 +54,7 @@ public class BallGame extends JFrame {
 
     public static void main(String[] args){
         System.out.println("2");
-        BallGame game = new BallGame();
+        BallGame2 game = new BallGame2();
         game.launchFrame();
     }
 }
